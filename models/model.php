7bfce9e11fe5879b3524ade8c53b9adde7688c55
Will Washburn <will.washburn@gmail.com>
2013-05-05 17:59:28 -0400

    add models

---
+++ models/model.php
@@ -0,0 +1,7 @@
+<?php
+
+class model {
+
+    protected $DBH;
+
+}
---
+++ models/schedule.php
@@ -0,0 +1,21 @@
+<?php
+
+    /*
+     *
+     * id
+     * user_id
+     * time_start
+     * time_end
+     * start_latitude
+     * start_longitude
+     * end_latitude
+     * created_at
+     */
+
+    class schedule extends model {
+
+
+
+
+
+    }
